8744b10a4f088107e35c8b0a983b64da3cf7066d
ahmed837363 <ahmed837363@users.noreply.github.com>
2025-10-12 10:22:46 +0300

    Add admin app - initial setup with login page

---
+++ admin-app/config.js
@@ -0,0 +1,8 @@
+﻿// Appwrite Configuration
+const APPWRITE_CONFIG = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: '6faeb3e2800390f4f7e577',
+    databaseId: '60ed4036002ab50c7171',
+    productsCollectionId: 'products',
+    storageBucketId: 'product-images'
+};
