f058f1cdb84fa90194fb02d16864fdb11dfa3525
ahmed837363 <ahmed837363@users.noreply.github.com>
2025-10-12 10:32:58 +0300

    Fix: Update to correct Project ID from Appwrite

--- admin-app/config.js
+++ admin-app/config.js
@@ -1,7 +1,7 @@
 ﻿// Appwrite Configuration
 const APPWRITE_CONFIG = {
     endpoint: 'https://cloud.appwrite.io/v1',
-    projectId: '6faeb3e2800390f4f7e577',
+    projectId: '65eb3e280039fdf7e677',
     databaseId: '60ed4036002ab50c7171',
     productsCollectionId: 'products',
     storageBucketId: 'product-images'
